5db277dfb8d721a6b13d6c0103f37d26787657c3
ishizaki <shinya.ishizaki.0311@gmail.com>
2025-09-01 13:52:00 +0900

    coded #01

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,12 +3,17 @@
 
 int main()
 {
-    double x{};
-    double y{};
+    double weight{};
+    double time{};
 
-    std::cin >> x;
-    std::cin >> y;
 
-	double z = x + y;
-    std::cout << z;
+	std::cout << "Enter weight in kilograms: ";
+    std::cin >> weight;
+
+	std::cout << "Enter time in minutes: ";
+    std::cin >> time;
+
+	double walking = 3 * weight * time *1.05 /60;
+    
+	std::cout << walking << " calories burned\n";
 }
