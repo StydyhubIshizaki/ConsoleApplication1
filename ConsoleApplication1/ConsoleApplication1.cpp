5a99bbb1b72678593212a5e76dbfae96029c879d
ishizaki <shinya.ishizaki.0311@gmail.com>
2025-09-05 14:52:27 +0900

    now on  coding #02

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,10 +16,24 @@ int add(int a, int b)
 	return a + b;
 }
 
+void sumPrint(int a)
+{
+	a = a + 20;
+	print(a);
+}
+
+int sample(int* a)
+{
+	print(a);
+	print(*a);
+	*a = *a + 20;
+	return *a;
+}
+
 int main()
 {
 	int x = 10;
-	int y = 20;
-	int z = add(x, y);
+	int z = sample(&x);
 	print(z);
+	print(x);
 }
