3594f216fa0d68dfd1e41cff317c4560fb81181a
ishizaki <shinya.ishizaki.0311@gmail.com>
2025-09-29 14:53:24 +0900

    now on trying a test in #02

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -37,11 +37,51 @@ int sample2(int& a)
 	return a;
 }
 
+//int main()
+//{
+//	int x = 10;
+//	//int z = sample(&x);
+//	int z = sample2(x);
+//	print(z);
+//	print(x);
+//}
+
 int main()
 {
-	int x = 10;
-	//int z = sample(&x);
-	int z = sample2(x);
-	print(z);
-	print(x);
+	std::string type;
+	int weight = 0;
+	int time = 0;
+	int calories = 0;
+
+	// 運動タイプを入力してもらう
+	std::cout << "運動タイプを入力してください（runnning / walking / cycling）" << std::endl;
+	std::cin >> type;
+
+	// 体重を入力してもらう
+	std::cout << "体重を入力してください" << std::endl;
+	std::cin >> weight;
+
+	// 運動時間（分数）を入力してもらう
+	std::cout << "運動時間（分数）を入力してください" << std::endl;
+	std::cin >> time;
+
+	// 運動タイプを判定する
+	if (type == "running")
+	{
+		calories = 9 * weight * time * 1.05 / 60;
+	}
+	else if (type == "walking")
+	{
+		calories = 3 * weight * time * 1.05 / 60;
+	}
+	else if (type == "cycling")
+	{
+		calories = 5 * weight * time * 1.05 / 60;
+	}
+	else
+	{
+		std::cout << "運動タイプを入力してください（runnning / walking / cycling）";
+	}
+
+	std::cout << "消費カロリーは" << calories << "です。" << std::endl;
 }
